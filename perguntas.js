75e4be971556e6634f2b24552e1996bd36162a42
Ana Luiza <148064048+analufelix@users.noreply.github.com>
2024-09-30 11:04:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+    'Filme',
+    'Qual foio primeiro filme animado de longa-metetragem já lançado?',
+    'Branca de Neve e o Sete Anoẽs'
+)
+
+criaCartao(
+    'Filme',
+    'Em o Mágico de Oz, qual é o nome do cachorro de Dorothy?',
+    'O nome é Totó'
+)
+
+criaCartao(
+    'Filme',
+    'Nos filmes transformes qual é o nome da faccção de robos vilões?',
+    'Decepticons'
+)
+
+criaCartao(
+    'Filme',
+    'Panter Negra se passa em qual país fictício',
+    'Wakanda'
+
+
+
+)
